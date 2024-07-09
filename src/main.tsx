1ac4fcaf46d812cf5f3012b8386b2ea9a05e83aa
VesperSpoony <714694113@qq.com>
2024-07-09 12:52:18 +0800

    day11

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client"
+// import App from "./App.tsx"
+// import App from "./useState-自动推导"
+// import App from "./useState-泛型参数"
+// import App from "./useState-初始值为null"
+// import App from "./props-基础使用"
+// import App from "./props-children"
+// import App from "./props-事件prop"
+import App from "./useRef"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />)
